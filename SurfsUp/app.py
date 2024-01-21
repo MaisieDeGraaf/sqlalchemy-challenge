b6678199dd7f9c86e825bc7fd85ad605747db692
Qwertykitten <146257477+MaisieDeGraaf@users.noreply.github.com>
2024-01-20 22:57:56 -0500

    Starting Flask

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,10 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime as dt
+import numpy as np
+import pandas as pd
+from sqlalchemy.ext.automap import automap_base
 
 #################################################
 # Database Setup
@@ -8,16 +12,19 @@
 
 
 # reflect an existing database into a new model
-
-# reflect the tables
-
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+hawaii_cxn = engine.connect()
 # Save references to each table
 
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+classes = Base.classes.keys()
+measurement_table = Base.classes.measurement
+station_table = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
-
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
